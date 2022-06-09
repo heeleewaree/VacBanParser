10f8af82dc372b6977210d734e1afef86b38f3c3
heeleewaree <78359736+heeleewaree@users.noreply.github.com>
2022-06-09 17:28:22 +0500

    Add files via upload

---
+++ VacBanParser/VacBanParser/General.Designer.cs
@@ -0,0 +1,241 @@
+﻿
+namespace VacBanParser
+{
+    partial class General
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle15 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle16 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle13 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle14 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnAccList = new System.Windows.Forms.Button();
+            this.btnParse = new System.Windows.Forms.Button();
+            this.pbGraph = new System.Windows.Forms.PictureBox();
+            this.tbCfg = new System.Windows.Forms.TextBox();
+            this.dgvMatrix = new System.Windows.Forms.DataGridView();
+            this.data = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.link = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vacStatus = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.comment = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGraph)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatrix)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAccList
+            // 
+            this.btnAccList.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAccList.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAccList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.btnAccList.ForeColor = System.Drawing.Color.White;
+            this.btnAccList.Location = new System.Drawing.Point(678, 11);
+            this.btnAccList.Name = "btnAccList";
+            this.btnAccList.Size = new System.Drawing.Size(94, 27);
+            this.btnAccList.TabIndex = 0;
+            this.btnAccList.Text = "Acc List";
+            this.btnAccList.UseVisualStyleBackColor = true;
+            this.btnAccList.Click += new System.EventHandler(this.btnAccList_Click);
+            // 
+            // btnParse
+            // 
+            this.btnParse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnParse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnParse.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.btnParse.ForeColor = System.Drawing.Color.White;
+            this.btnParse.Location = new System.Drawing.Point(653, 523);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(119, 26);
+            this.btnParse.TabIndex = 1;
+            this.btnParse.Text = "Parse and Save";
+            this.btnParse.UseVisualStyleBackColor = true;
+            this.btnParse.Click += new System.EventHandler(this.btnParse_Click);
+            // 
+            // pbGraph
+            // 
+            this.pbGraph.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbGraph.Location = new System.Drawing.Point(0, 0);
+            this.pbGraph.Name = "pbGraph";
+            this.pbGraph.Size = new System.Drawing.Size(784, 561);
+            this.pbGraph.TabIndex = 3;
+            this.pbGraph.TabStop = false;
+            // 
+            // tbCfg
+            // 
+            this.tbCfg.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.tbCfg.Enabled = false;
+            this.tbCfg.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.tbCfg.ForeColor = System.Drawing.Color.White;
+            this.tbCfg.Location = new System.Drawing.Point(12, 11);
+            this.tbCfg.Name = "tbCfg";
+            this.tbCfg.Size = new System.Drawing.Size(660, 27);
+            this.tbCfg.TabIndex = 4;
+            this.tbCfg.Text = "C:\\VacBan Parser";
+            // 
+            // dgvMatrix
+            // 
+            dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            dataGridViewCellStyle9.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle9.NullValue = null;
+            this.dgvMatrix.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle9;
+            this.dgvMatrix.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.dgvMatrix.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle10.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle10.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle10.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvMatrix.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle10;
+            this.dgvMatrix.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.data,
+            this.link,
+            this.vacStatus,
+            this.comment});
+            this.dgvMatrix.GridColor = System.Drawing.Color.White;
+            this.dgvMatrix.Location = new System.Drawing.Point(12, 70);
+            this.dgvMatrix.Name = "dgvMatrix";
+            dataGridViewCellStyle15.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle15.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            dataGridViewCellStyle15.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle15.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle15.SelectionBackColor = System.Drawing.SystemColors.ControlLightLight;
+            dataGridViewCellStyle15.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle15.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvMatrix.RowHeadersDefaultCellStyle = dataGridViewCellStyle15;
+            dataGridViewCellStyle16.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle16.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            dataGridViewCellStyle16.ForeColor = System.Drawing.Color.White;
+            this.dgvMatrix.RowsDefaultCellStyle = dataGridViewCellStyle16;
+            this.dgvMatrix.Size = new System.Drawing.Size(594, 448);
+            this.dgvMatrix.TabIndex = 5;
+            // 
+            // data
+            // 
+            dataGridViewCellStyle11.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            dataGridViewCellStyle11.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle11.NullValue = null;
+            this.data.DefaultCellStyle = dataGridViewCellStyle11;
+            this.data.HeaderText = "Data";
+            this.data.Name = "data";
+            this.data.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.data.Width = 70;
+            // 
+            // link
+            // 
+            dataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            dataGridViewCellStyle12.ForeColor = System.Drawing.Color.White;
+            this.link.DefaultCellStyle = dataGridViewCellStyle12;
+            this.link.HeaderText = "Link";
+            this.link.Name = "link";
+            this.link.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.link.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.link.Width = 250;
+            // 
+            // vacStatus
+            // 
+            dataGridViewCellStyle13.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle13.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            dataGridViewCellStyle13.ForeColor = System.Drawing.Color.White;
+            this.vacStatus.DefaultCellStyle = dataGridViewCellStyle13;
+            this.vacStatus.HeaderText = "Vac Status";
+            this.vacStatus.Name = "vacStatus";
+            this.vacStatus.ReadOnly = true;
+            this.vacStatus.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.vacStatus.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.vacStatus.Width = 80;
+            // 
+            // comment
+            // 
+            this.comment.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle14.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle14.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            dataGridViewCellStyle14.ForeColor = System.Drawing.Color.White;
+            this.comment.DefaultCellStyle = dataGridViewCellStyle14;
+            this.comment.HeaderText = "Comment";
+            this.comment.Name = "comment";
+            this.comment.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.btnRefresh.ForeColor = System.Drawing.Color.White;
+            this.btnRefresh.Location = new System.Drawing.Point(678, 44);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(94, 27);
+            this.btnRefresh.TabIndex = 6;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // General
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.dgvMatrix);
+            this.Controls.Add(this.tbCfg);
+            this.Controls.Add(this.btnParse);
+            this.Controls.Add(this.btnAccList);
+            this.Controls.Add(this.pbGraph);
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "General";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VacBan Parser";
+            ((System.ComponentModel.ISupportInitialize)(this.pbGraph)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatrix)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAccList;
+        private System.Windows.Forms.Button btnParse;
+        public System.Windows.Forms.PictureBox pbGraph;
+        private System.Windows.Forms.TextBox tbCfg;
+        private System.Windows.Forms.DataGridView dgvMatrix;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn data;
+        private System.Windows.Forms.DataGridViewTextBoxColumn link;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vacStatus;
+        private System.Windows.Forms.DataGridViewTextBoxColumn comment;
+    }
+}
+
---
+++ VacBanParser/VacBanParser/General.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Net;
+using System.Drawing;
+
+namespace VacBanParser
+{
+    public partial class General : Form
+    {
+        public General()
+        {
+            InitializeComponent();
+            dgvMatrix.RowCount = maxAccValue;
+            InitialDesign();
+        }
+
+        #region Global Vars
+        static int maxAccValue = 10000;
+        Stopwatch stopWatch = new Stopwatch();
+        String[] accList = new String[maxAccValue];
+        String[] bans = new String[maxAccValue];
+        String[] matrixData = new String[maxAccValue];
+        String[] matrixLink = new String[maxAccValue];
+        String[] matrixComment = new String[maxAccValue];
+        double[] daysAfterDate = new double[maxAccValue];
+        int totalPlayers = 0;
+        int totalBans = 0;
+        double time;
+        #endregion
+
+        #region Button AccList
+        private void btnAccList_Click(object sender, EventArgs e)
+        {
+            Process cfg = new Process();
+            cfg.StartInfo.FileName = "notepad.exe";
+            cfg.StartInfo.Arguments = @"C:\VacBan Parser\AccList.cfg";
+            cfg.Start();
+        }
+        #endregion
+
+        #region Button Refresh
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            InitialDesign();
+        }
+        #endregion
+
+        #region Button Parse and Save
+        private void btnParse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileSaving();
+                FileReading();
+                stopWatch.Start();
+                Parallel.ForEach(Iterate(0, totalPlayers - 1, 1), j =>
+                {
+                    WebClient webclient = new WebClient();
+                    String profileID = webclient.DownloadString(accList[j]);
+                    String days = "";
+                    String daysReversed = "";
+                    bool res = profileID.Contains("day(s) since last ban");
+                    if (res)
+                    {
+                        int pos = profileID.IndexOf("day(s) since last ban") - 2;
+                        while (profileID[pos] != '\t')
+                        {
+                            days += profileID[pos];
+                            pos--;
+                        }
+                        for (int i = days.Length - 1; i > -1; i--)
+                        {
+                            daysReversed += days[i];
+                        }
+                    }
+                    bans[j] = daysReversed;
+                    int day = 0, month = 0, year = 0;
+                    string dayF = "", monthF = "", yearF = "";
+                    int n = 0;
+                    String tim = Convert.ToString(dgvMatrix.Rows[j].Cells[0].Value);
+                    if (tim != "")
+                    {
+                        for (int p = 0; p < tim.Length; p++)
+                        {
+                            if ((tim[p] == '.') || (tim[p] == ',') || (tim[p] == '/') || (tim[p] == ' '))
+                            {
+                                if ((month == 0) && (n != 0))
+                                {
+                                    for (int v = n + 1; v < p; v++)
+                                        monthF += tim[v];
+                                    month = Convert.ToInt32(monthF);
+                                    n = p;
+                                }
+                                if (day == 0)
+                                {
+                                    for (int v = 0; v < p; v++)
+                                        dayF += tim[v];
+                                    day = Convert.ToInt32(dayF);
+                                    n = p;
+                                }
+                            }
+                        }
+                        if ((year == 0) && (n != 0))
+                        {
+                            for (int v = n + 1; v < tim.Length; v++)
+                                yearF += tim[v];
+                            year = Convert.ToInt32(yearF);
+                        }
+                        DateTime data = new DateTime(year, month, day);
+                        daysAfterDate[j] = (DateTime.Today - data).TotalDays;
+                        if (bans[j] != "")
+                        {
+                            if (daysAfterDate[j] > Convert.ToInt32(bans[j]))
+                                dgvMatrix.Rows[j].Cells[2].Value = bans[j];
+                        }
+                    }
+                });
+                stopWatch.Stop();
+                time = stopWatch.ElapsedMilliseconds;
+                stopWatch.Reset();
+                totalBans = 0;
+                for (int i = 0; i < totalPlayers - 1; i++)
+                {
+                    if ((bans[i] != "") && (daysAfterDate[i] > Convert.ToInt32(bans[i])))
+                        totalBans++;
+                }
+                paintGraph();
+            }
+            catch
+            {
+                return;
+            }
+        }
+        #endregion
+
+        #region Paint Function
+        public void paintGraph()
+        {
+            Bitmap btmp = new Bitmap(this.Width, this.Height);
+            Graphics gr = Graphics.FromImage(btmp);
+            pbGraph.Image = btmp;
+            int X = 300 + this.Width / 2;
+            int Y = -130 + this.Height / 2;
+            float bannedUsers = 360.0f * totalBans / (totalPlayers - 1);
+            gr.FillPie(new SolidBrush(Color.FromArgb(200, 72, 71)), new Rectangle(X - 75, Y - 75, 150, 150), 90.0f, bannedUsers);
+            gr.FillPie(new SolidBrush(Color.FromArgb(100, 149, 237)), new Rectangle(X - 75, Y - 75, 150, 150), 90.0f + bannedUsers, 360.0f - (0.0f + bannedUsers));
+
+            gr.FillRectangle(new SolidBrush(Color.FromArgb(200, 72, 71)), new Rectangle(X - 75 + 20, Y + 100, 15, 15));
+            gr.DrawString("Banned: " + totalBans, new Font("Arial", 8), new SolidBrush(Color.White), new Point(X - 55 + 20, Y + 100));
+            gr.FillRectangle(new SolidBrush(Color.FromArgb(100, 149, 237)), new Rectangle(X - 75 + 20, Y + 120, 15, 15));
+            gr.DrawString("Not banned: " + ((totalPlayers - 1) - totalBans), new Font("Arial", 8), new SolidBrush(Color.White), new Point(X - 55 + 20, Y + 120));
+
+            gr.DrawString("Total players: " + (totalPlayers - 1), new Font("Arial", 8), new SolidBrush(Color.White), new Point(X - 75 + 20, Y + 140));
+            gr.DrawString("Percentage: " + Math.Round((1.0 * totalBans / (totalPlayers - 1)) * 100.0, 3) + " %", new Font("Arial", 8), new SolidBrush(Color.White), new Point(X - 75 + 20, Y + 160));
+
+            gr.DrawString("time: " + (time / 1000.0) + " sec", new Font("Arial", 8), new SolidBrush(Color.White), new Point(10, 540));
+
+
+
+
+            gr.Dispose();
+        }
+        #endregion
+
+        #region Iterate Function
+        private IEnumerable<int> Iterate(int x, int y, int a)
+        {
+            for (int i = x; i < y; i += a)
+                yield return i;
+        }
+        #endregion
+
+        #region File Reading
+        private void FileReading()
+        {
+            totalPlayers = 1;
+            try
+            {
+                StreamReader file = new StreamReader(@"C:\VacBan Parser\AccList.cfg");
+                accList[0] = file.ReadLine();
+                while (accList[totalPlayers - 1] != "")
+                {
+                    accList[totalPlayers] = file.ReadLine();
+                    totalPlayers++;
+                }
+                file.Close();
+            }
+            catch
+            {
+                return;
+            }
+        }
+        #endregion
+
+        #region File Saving
+        public void FileSaving()
+        {
+            try
+            {
+                StreamWriter file = new StreamWriter(@"C:\VacBan Parser\MatrixData.cfg");
+                for (int i = 0; i < dgvMatrix.RowCount; i++)
+                    file.WriteLine(dgvMatrix.Rows[i].Cells[0].Value);
+
+                file.Close();
+            }
+            catch
+            {
+                return;
+            }
+
+            try
+            {
+                StreamWriter file = new StreamWriter(@"C:\VacBan Parser\AccList.cfg");
+                for (int i = 0; i < dgvMatrix.RowCount; i++)
+                    file.WriteLine(dgvMatrix.Rows[i].Cells[1].Value);
+
+                file.Close();
+            }
+            catch
+            {
+                return;
+            }
+
+            try
+            {
+                StreamWriter file = new StreamWriter(@"C:\VacBan Parser\MatrixComment.cfg");
+                for (int i = 0; i < dgvMatrix.RowCount; i++)
+                    file.WriteLine(dgvMatrix.Rows[i].Cells[3].Value);
+
+                file.Close();
+            }
+            catch
+            {
+                return;
+            }
+        }
+        #endregion
+
+        #region Initial Design
+        public void InitialDesign()
+        {
+            int i = 1;
+            try
+            {
+                StreamReader file = new StreamReader(@"C:\VacBan Parser\MatrixData.cfg");
+                matrixData[0] = file.ReadLine();
+                while (matrixData[i - 1] != "")
+                {
+                    matrixData[i] = file.ReadLine();
+                    dgvMatrix.Rows[i - 1].Cells[0].Value = matrixData[i - 1];
+                    i++;
+                }
+                file.Close();
+            }
+            catch
+            {
+                return;
+            }
+
+            i = 1;
+            try
+            {
+                totalPlayers = 1;
+                StreamReader file = new StreamReader(@"C:\VacBan Parser\AccList.cfg");
+                matrixLink[0] = file.ReadLine();
+                while (matrixLink[i - 1] != "")
+                {
+                    matrixLink[i] = file.ReadLine();
+                    dgvMatrix.Rows[i - 1].Cells[1].Value = matrixLink[i - 1];
+                    accList[i - 1] = matrixLink[i - 1];
+                    i++;
+                    totalPlayers++;
+                }
+                file.Close();
+            }
+            catch
+            {
+                return;
+            }
+
+            i = 1;
+            try
+            {
+                StreamReader file = new StreamReader(@"C:\VacBan Parser\MatrixComment.cfg");
+                matrixComment[0] = file.ReadLine();
+                while (matrixComment[i - 1] != "")
+                {
+                    matrixComment[i] = file.ReadLine();
+                    dgvMatrix.Rows[i - 1].Cells[3].Value = matrixComment[i - 1];
+                    i++;
+                }
+                file.Close();
+            }
+            catch
+            {
+                return;
+            }
+        }
+        #endregion
+    }
+}
